a6bda56bb963c3a3fe670631e802a40d0cc3fa9c
Andrew Pacifico <andrewcpacifico@gmail.com>
2017-01-02 11:41:27 -0400

    Hugo version

--- README.md
+++
@@ -1,1 +0,0 @@
-#### A blog for software developers.
--- grunt/workflow.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(grunt) {
-    grunt.registerTask('dev', ['jekyll', 'compass:dev', 'copy', 'concurrent']);
-    grunt.registerTask('build', [ 'jekyll', 'compass:dist', 'copy:vendor', 'uglify', 'htmlmin']);
-};
